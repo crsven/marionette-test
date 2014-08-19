311313c1756be0de6dc83cebfcc036cbd9b53d6c
Chris Svenningsen <chris@carbonfive.com>
2014-08-19 15:42:21 -0700

    Remove log from server.

---
+++ server/chris-example-server.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require 'sinatra/cross_origin'
+require 'json'
+
+configure do
+  enable :cross_origin
+end
+
+post '/apply' do
+  content_type :json
+  return [
+    { name: params[:first_name], rate: '3.4' },
+    { name: 'ok', rate: '4.4' },
+    { name: 'decent', rate: '5.4' },
+    { name: params[:last_name], rate: '6.4' },
+    { name: 'worse', rate: '7.4' },
+    { name: 'worst', rate: '8.4' },
+  ].to_json
+end
